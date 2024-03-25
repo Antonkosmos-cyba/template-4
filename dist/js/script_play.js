27d6a625679cad0d99e7069ee4f232c32b7ab903
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-03-25 18:38:54 +0300

    muz

--- dist/js/script_play.js
+++ dist/js/script_play.js
@@ -1,21 +1,24 @@
-document.getElementById('play1').onclick = function () {
+document.getElementById("play1").onclick = function () {
   document.getElementById("demo1").play();
 };
-document.getElementById('pause1').onclick = function () {
+document.getElementById("pause1").onclick = function () {
   document.getElementById("demo1").pause();
 };
 
-document.getElementById('play2').onclick = function () {
+//
+
+document.getElementById("play2").onclick = function () {
   document.getElementById("demo2").play();
 };
-document.getElementById('pause2').onclick = function () {
+document.getElementById("pause2").onclick = function () {
   document.getElementById("demo2").pause();
 };
 
-document.getElementById('play3').onclick = function () {
+//
+
+document.getElementById("play3").onclick = function () {
   document.getElementById("demo3").play();
 };
-document.getElementById('pause3').onclick = function () {
+document.getElementById("pause3").onclick = function () {
   document.getElementById("demo3").pause();
-}
-
+};
